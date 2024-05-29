23867674d4e3bcce1d42edbb9c5dd8b9e5f93162
Alp Kaynar <mehmetalpkaynar@gmail.com>
2024-05-29 22:58:33 +0300

    linked front back*

--- network_automation/views.py
+++ network_automation/views.py
@@ -7,7 +7,87 @@ from .models import NetworkDevice
 class DeviceListView(LoginRequiredMixin, View):
     def get(self, request):
         devices = NetworkDevice.objects.filter(user=request.user)
-        return render(request, 'device_list.html', {'devices': devices})
+        nodes = []
+        edges = []
+
+
+        # BUNU SILMEYIN LUTFEN OLMASI GEREKN BU
+
+        # for device in devices:
+        #     node = {
+        #         'id': device.id,
+        #         'label': device.name,
+        #         'title': f"{device.name}\nIP: {device.ip_address}\nSSH: {device.ssh_username}",
+        #         'image': device.image.url if device.image else '',
+        #         'shape': 'image',
+        #         'size': 50,
+        #     }
+        #     nodes.append(node)
+        #
+        #     for connected_device in device.connected_devices.all():
+        #         edge = {
+        #             'from': device.id,
+        #             'to': connected_device.id,
+        #         }
+        #         edges.append(edge)
+
+
+
+        nodes = [
+            {'id': 1, 'label': 'Backbone Switch', 'title': 'Backbone Switch\nIP: 10.0.0.1\nSSH: admin',
+             'image': '/static/images/backbone.jpg', 'shape': 'image', 'size': 30},
+            {'id': 2, 'label': 'Switch 1', 'title': 'Switch 1\nIP: 10.0.1.1\nSSH: admin',
+             'image': '/static/images/Switch.png', 'shape': 'image', 'size': 50},
+            {'id': 3, 'label': 'Switch 2', 'title': 'Switch 2\nIP: 10.0.2.1\nSSH: admin',
+             'image': '/static/images/Switch.png', 'shape': 'image', 'size': 50},
+            {'id': 4, 'label': 'Switch 3', 'title': 'Switch 3\nIP: 10.0.3.1\nSSH: admin',
+             'image': '/static/images/Switch.png', 'shape': 'image', 'size': 50},
+            {'id': 5, 'label': 'PC 1-1', 'title': 'PC 1-1\nIP: 10.0.1.2\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 6, 'label': 'PC 1-2', 'title': 'PC 1-2\nIP: 10.0.1.3\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 7, 'label': 'PC 1-3', 'title': 'PC 1-3\nIP: 10.0.1.4\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 8, 'label': 'PC 2-1', 'title': 'PC 2-1\nIP: 10.0.2.2\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 9, 'label': 'PC 2-2', 'title': 'PC 2-2\nIP: 10.0.2.3\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 10, 'label': 'PC 2-3', 'title': 'PC 2-3\nIP: 10.0.2.4\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 11, 'label': 'PC 3-1', 'title': 'PC 3-1\nIP: 10.0.3.2\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 12, 'label': 'PC 3-2', 'title': 'PC 3-2\nIP: 10.0.3.3\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30},
+            {'id': 13, 'label': 'PC 3-3', 'title': 'PC 3-3\nIP: 10.0.3.4\nSSH: user', 'image': '/static/images/pc.jpg',
+             'shape': 'image', 'size': 30}
+        ]
+
+        edges = [
+            {'from': 1, 'to': 2},
+            {'from': 1, 'to': 3},
+            {'from': 1, 'to': 4},
+            {'from': 2, 'to': 5},
+            {'from': 2, 'to': 6},
+            {'from': 2, 'to': 7},
+            {'from': 3, 'to': 8},
+            {'from': 3, 'to': 9},
+            {'from': 3, 'to': 10},
+            {'from': 4, 'to': 11},
+            {'from': 4, 'to': 12},
+            {'from': 4, 'to': 13}
+        ]
+
+        context = {
+            'nodes': nodes,
+            'edges': edges,
+        }
+
+        context = {
+            'nodes': nodes,
+            'edges': edges,
+        }
+        return render(request, 'device_list.html', context)
+
 
 class DeviceBackupView(LoginRequiredMixin, View):
     def post(self, request, device_id):
